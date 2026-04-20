ac7522d60cac27f5e222326779c192c673d7d5f1
mayue <mayuetmy@163.com>
2026-04-20 23:26:37 +0800

    dyntablename

--- app.py
+++ app.py
@@ -21,6 +21,29 @@ headers = {
     "xc-token": NC_TOKEN,
     "Content-Type": "application/json"
 }
+def get_column_id_by_name(base_id, table_id, target_column_name):
+    """
+    通过字段的显示名称获取其在 API 中使用的 ID (title)
+    """
+    #https://app.nocodb.com/api/v3/meta/bases/{baseId}/tables/{tableId}
+    meta_url = f"{NC_BASE_URL}/api/v3/meta/bases/{base_id}/tables/{table_id}"
+    
+    try:
+        response = requests.get(meta_url, headers=headers)
+        if response.status_code == 200:
+            payload = response.json()
+            fields = payload.get("fields")
+            # 在返回的列列表中查找匹配的显示名称 (label)
+            for col in fields:
+                if col.get("title") == target_column_name:
+                    # 返回字段标识符，通常是类似 'nc_xxxx' 或 'ColumnID'
+                    return col.get("id") 
+            print(f"未找到名称为 {target_column_name} 的字段")
+        else:
+            print(f"获取元数据失败: {response.text}")
+    except Exception as e:
+        print(f"请求异常: {e}")
+    return "_null"
 
 def process_link(payload):
     """通用关联逻辑：将源表 ID 关联到目标表的指定字段"""
@@ -36,7 +59,8 @@ def process_link(payload):
     # 1. 拿到源表新产生的 ID
     source_record_id = rows[0].get("Id")
     source_table_id = data.get("table_name", "_null");
-    link_field_id = str(os.getenv(f"LINK_FIELD_{source_table_id}"));
+    link_field_id = get_column_id_by_name(BASE_ID,TARGET_TABLE_ID, source_table_id)
+    #str(os.getenv(f"LINK_FIELD_{source_table_id}"));
     # https://app.nocodb.com/api/v3/data/{baseId}/{tableId}/links/{linkFieldId}/{recordId}
     # 2. 构造 PATCH 请求，修改目标表（B表）的第 1 条记录
     patch_url = f"{NC_BASE_URL}/api/v3/data/{BASE_ID}/{TARGET_TABLE_ID}/links/{link_field_id}/{FIXED_TARGET_RECORD_ID}"
@@ -65,7 +89,7 @@ def webhook_a2026():
     # 假设 A2026 对应字段 ID: cbtt8iets0ukc07
     return process_link(request.json)
 
-# @app.route('/nocodbwebhook/sa2026', methods=['POST'])
+# @app.route('/nocodbwebhook/sa2026', methods=['ddOST'])
 # def webhook_sa2026():
 #     # 假设 SA2026 对应另一个字段 ID
 #     return process_link(request.json, "croguwkyp3584wc")
